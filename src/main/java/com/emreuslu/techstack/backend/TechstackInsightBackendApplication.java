e2db59afb4d012d3260d8a6c57b3d0e9db79a3eb
Emre Uslu <emreuslu256@gmail.com>
2026-04-17 18:22:44 +0300

    initial commit

---
+++ src/main/java/com/emreuslu/techstack/backend/TechstackInsightBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emreuslu.techstack.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechstackInsightBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechstackInsightBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emreuslu/techstack/backend/TechstackInsightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emreuslu.techstack.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechstackInsightBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
